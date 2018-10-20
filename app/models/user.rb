62e069670dace2134055e78450ca4a44989b3d5d
ya-mac <cu.cs.y20@gmail.com>
2018-10-20 23:06:54 +0900

    14.3 Add status feed using subselects

--- app/models/user.rb
+++ app/models/user.rb
@@ -75,10 +75,10 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # feed (beta)
-  # full version will be coming with next chapter
+  # return user's status feed
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # follow user
